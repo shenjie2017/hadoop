540a2d9e028291ae1f4587e9d9d3a4ccf8baedfc
shenjie <1075850619@qq.com>
2018-01-12 17:54:49 +0800

    新增hdfs流上传和下载

--- hdfs/hdfs-demo/src/test/java/com/blue/hadoop/hdfs/HDFSTestCase.java
+++ hdfs/hdfs-demo/src/test/java/com/blue/hadoop/hdfs/HDFSTestCase.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author shenjie
@@ -37,10 +39,11 @@ import java.net.URISyntaxException;
 
 public class HDFSTestCase {
     private FileSystem fs = null;
+    private Configuration conf = null;
 
     @Before
     public void init() throws Exception {
-        Configuration conf = new Configuration();
+        conf = new Configuration();
 //        conf.set("hadoop.home.dir", "D:/develop/hadoop-3.0.0");
 //        conf.set("fs.default.name","hdfs://192.168.163.111:9000");
         fs = FileSystem.get(new URI("hdfs://192.168.163.111:9000"),conf,"hadoop");
@@ -66,5 +69,17 @@ public class HDFSTestCase {
         fs.copyToLocalFile(false,new Path("/software/jdk-8u151-linux-x64.tar.gz"),new Path("D:/software/jdk-8u151-linux-x64.tar.gz.copy"),false);
         fs.close();
         System.out.println("download success!!");
+    }
+
+    /**
+     * 查看conf的默认参数
+     */
+    @Test
+    public void testConf(){
+        Iterator<Map.Entry<String,String>> iterator = conf.iterator();
+        while(iterator.hasNext()){
+            Map.Entry<String,String> entry = iterator.next();
+            System.out.println("key:"+entry.getKey()+"\t"+"value:"+entry.getValue());
+        }
     }
 }
